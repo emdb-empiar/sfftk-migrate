886725df51b8289184cb37c725f2e12d3afd8fbc
paulkorir <pkorir@ebi.ac.uk>
2019-11-14 15:49:23 +0000

    First read of `.xsl` file and trivial transform

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sfftk-migrate',
+    version='0.1.0.dev0',
+    packages=['sfftk_migrate'],
+    url='',
+    license='Apache 2.0',
+    author='Paul K. Korir, PhD',
+    author_email='pkorir@ebi.ac.uk, paul.korir@gmail.com',
+    description='Migrate older EMDB-SFF files to newer versions',
+    install_requires=['lxml'],
+)
